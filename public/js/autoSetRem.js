1a03aa7c2bc98fa8aa84bedd9f9e9f97853c6abf
三页 <825171433@qq.com>
2017-07-10 18:14:28 +0800

    dataShowH5页面展示

---
+++ public/js/autoSetRem.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Yy on 2017/7/10.
+ */
+var autoSetRem = (function(doc, win) {
+    var docEl = doc.documentElement,
+        done = false,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        set = function(paper) {
+            if(paper==null){
+                paper=750;
+            }
+            var clientWidth = docEl.clientWidth || win.innerWidth || screen.width;
+            if(clientWidth>=640){
+                clientWidth = 640;
+            }
+            if (!clientWidth) return;
+            docEl.style.fontSize = 100 * (clientWidth / paper) + 'px';
+            done = true;
+            return done;
+        };
+    return {
+        set:set
+    };
+    //AbortifbrowserdoesnotsupportaddEventListener
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, set, false);
+    doc.addEventListener('DOMContentLoaded', set, false);
+})(document, window);
+
+autoSetRem.set(750);
